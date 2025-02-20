1be402f10bac0c61be52778d91b37e6c36bc71ac
kekehurry <arhukai1111@gmail.com>
2025-02-20 16:30:08 +0800

    init

---
+++ update_menu.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+# 修改为你的仓库信息
+GITHUB_USER = "kekehurry"
+REPO_NAME = "cityflow_community"
+BRANCH = "main"
+BASE_URL = f"https://raw.githubusercontent.com/{GITHUB_USER}/{REPO_NAME}/refs/heads/{BRANCH}/cityflow_database/json"
+
+workflows = {}
+
+for d in os.listdir(os.getcwd()):
+    if os.path.isdir(d):
+        folder_path = os.path.join(os.getcwd(), d)
+        if os.path.exists(folder_path):
+            files = []
+            for file in os.listdir(folder_path):
+                if file.endswith(".csflow.json"):
+                    url = f"{BASE_URL}/{d}/{file}"
+                    files.append(url)
+            # 排序可选
+            workflows[d] = sorted(files)
+        else:
+            workflows[d] = []
+
+with open("community_workflows.json", "w", encoding="utf-8") as f:
+    json.dump(workflows, f, ensure_ascii=False, indent=2)
