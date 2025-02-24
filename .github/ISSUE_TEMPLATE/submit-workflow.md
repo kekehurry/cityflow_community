3746ad3a6f906a52bd9796d5066e261464bce31b
kekehurry <arhukai1111@gmail.com>
2025-02-24 16:05:49 +0800

    add submit template

---
+++ .github/ISSUE_TEMPLATE/submit-workflow.md
@@ -0,0 +1,18 @@
+---
+name: Submit File
+about: Submit a file for review and inclusion in the repository
+title: "[WorkFlow Submission]"
+labels: workflow-submission
+---
+
+### Author
+The aothor name.
+
+### Category
+The category of the workflow.
+
+### Description
+Please describe the file you are submitting.
+
+### WorkFlow File
+Attach your file by dragging and dropping it here or clicking to upload.
--- update_menu.py
+++ update_menu.py
@@ -8,12 +8,13 @@ source_folder = os.path.join(os.getcwd(), 'source')
 workflows_folder = os.path.join(os.getcwd(), 'workflows')
 base_url = 'https://kekehurry.github.io/cityflow_community'
 
-def load_file(file_path,category):
+def load_file(file_path,author,category):
     file = os.path.basename(file_path)
     with open('community_workflows.json', "r", encoding="utf-8") as f:
         workflows = json.load(f)
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
+    data['author'] = author
     data = save_workflow(data, source_folder, base_url)
     ouput_folder = os.path.join(workflows_folder, category)
     if not os.path.exists(ouput_folder):
@@ -30,9 +31,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Update community workflows.')
     parser.add_argument('--category', type=str, required=True, help='The category of the workflow.')
     parser.add_argument('--file', type=str, required=True, help='The path to the workflow file.')
+    parser.add_argument('--author',type=str, required=True, help='The author of the workflow file.')
     args = parser.parse_args()
     category = args.category
     file_path = args.file
-    workflows = load_file(file_path,category)
+    author = args.author
+    workflows = load_file(file_path,author,category)
     with open('community_workflows.json', "w", encoding="utf-8") as f:
         json.dump(workflows, f, ensure_ascii=False, indent=2)
