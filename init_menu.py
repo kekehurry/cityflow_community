1e70647a8e4f955e284b3653ca40ae698c983c11
kekehurry <arhukai1111@gmail.com>
2025-02-24 14:14:29 +0800

    change init workflows

---
+++ init_menu.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import json
+from utils.save import save_workflow
+
+
+
+source_folder = os.path.join(os.getcwd(), 'source')
+raw_folder = os.path.join(os.getcwd(), 'raw')
+workflows_folder = os.path.join(os.getcwd(), 'workflows')
+
+if os.path.exists(source_folder):
+    shutil.rmtree(source_folder)
+    
+for folder in ['files','icons','images','html']:
+    if not os.path.exists(os.path.join(source_folder,folder)): 
+        os.makedirs(os.path.join(source_folder,folder))
+
+base_url = 'https://kekehurry.github.io/cityflow_community'
+
+
+workflows = {}
+for d in os.listdir(raw_folder):
+    folder_path = os.path.join(raw_folder, d)
+    if os.path.isdir(folder_path):
+        files = []
+        for file in os.listdir(folder_path):
+            if file.endswith(".csflow.json"):
+                file_path = os.path.join(folder_path, file)
+                with open(file_path, "r", encoding="utf-8") as f:
+                    data = json.load(f)
+                    data = save_workflow(data, source_folder, base_url)
+                ouput_folder = os.path.join(workflows_folder, d)
+                if not os.path.exists(ouput_folder):
+                    os.makedirs(ouput_folder)
+                with open(os.path.join(ouput_folder, file), "w", encoding="utf-8") as f:
+                    json.dump(data, f, ensure_ascii=False, indent=2)
+                files.append(base_url+f"/workflows/{d}/{file}")
+        # 排序可选
+        workflows[d] = sorted(files)
+
+with open('community_workflows.json', "w", encoding="utf-8") as f:
+    json.dump(workflows, f, ensure_ascii=False, indent=2)
--- update_menu.py
+++ update_menu.py
@@ -1,43 +1,38 @@
 import os
-import shutil
 import json
 from utils.save import save_workflow
-
+import argparse
 
 
 source_folder = os.path.join(os.getcwd(), 'source')
-raw_folder = os.path.join(os.getcwd(), 'raw')
 workflows_folder = os.path.join(os.getcwd(), 'workflows')
-
-if os.path.exists(source_folder):
-    shutil.rmtree(source_folder)
-    
-for folder in ['files','icons','images','html']:
-    if not os.path.exists(os.path.join(source_folder,folder)): 
-        os.makedirs(os.path.join(source_folder,folder))
-
 base_url = 'https://kekehurry.github.io/cityflow_community'
 
+def load_file(file_path,category):
+    file = os.path.basename(file_path)
+    with open('community_workflows.json', "r", encoding="utf-8") as f:
+        workflows = json.load(f)
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    data = save_workflow(data, source_folder, base_url)
+    ouput_folder = os.path.join(workflows_folder, category)
+    if not os.path.exists(ouput_folder):
+        os.makedirs(ouput_folder)
+    with open(os.path.join(ouput_folder, file), "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+    if category not in workflows:
+        workflows[category] = []
+    workflows[category].append(base_url+f"/workflows/{category}/{file}")
+    return workflows
 
-workflows = {}
-for d in os.listdir(raw_folder):
-    folder_path = os.path.join(raw_folder, d)
-    if os.path.isdir(folder_path):
-        files = []
-        for file in os.listdir(folder_path):
-            if file.endswith(".csflow.json"):
-                file_path = os.path.join(folder_path, file)
-                with open(file_path, "r", encoding="utf-8") as f:
-                    data = json.load(f)
-                    data = save_workflow(data, source_folder, base_url)
-                ouput_folder = os.path.join(workflows_folder, d)
-                if not os.path.exists(ouput_folder):
-                    os.makedirs(ouput_folder)
-                with open(os.path.join(ouput_folder, file), "w", encoding="utf-8") as f:
-                    json.dump(data, f, ensure_ascii=False, indent=2)
-                files.append(base_url+f"/workflows/{d}/{file}")
-        # 排序可选
-        workflows[d] = sorted(files)
-
-with open('community_workflows.json', "w", encoding="utf-8") as f:
-    json.dump(workflows, f, ensure_ascii=False, indent=2)
+if __name__ == '__main__':
+    # parse parameters from command line use --category and --file
+    parser = argparse.ArgumentParser(description='Update community workflows.')
+    parser.add_argument('--category', type=str, required=True, help='The category of the workflow.')
+    parser.add_argument('--file', type=str, required=True, help='The path to the workflow file.')
+    args = parser.parse_args()
+    category = args.category
+    file_path = args.file
+    workflows = load_file(file_path,category)
+    with open('community_workflows.json', "w", encoding="utf-8") as f:
+        json.dump(workflows, f, ensure_ascii=False, indent=2)
